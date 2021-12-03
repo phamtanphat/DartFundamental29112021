af242ff6bb6759341a69f7096a70db7ac73a5434
phamtanphat <phatdroid94@gmail.com>
2021-12-03 20:50:19 +0700

    final vs const

--- lib/buoi1.dart
+++ lib/buoi1.dart
@@ -36,6 +36,32 @@ void main(List<String> args) {
 
    String info = "Name : $name , age : ${age + 1}";
    print(info);
+
+   4 : Multiple line
+
+   String multipleLine =
+      'SELECT * '
+      'FROM table_name'
+      'Where id = 1';
+
+  print(multipleLine);
+
+  var a = 3.15;
+  final data1 = a;
+  const data2 =3.15;
+
+
+  const chỉ gán được cho các giá trị có sẳn
+  final gán được bất kỳ giá trị nào
+
+  var a = 10; // a tạo ra ở runtime
+  final PI = a;
+
+  const PI1 = a;
    */
+
+
+
+
 
 }
