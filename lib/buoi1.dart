60183a81d14381b96abff86303ad1b0c807b6b29
phamtanphat <phatdroid94@gmail.com>
2021-12-03 20:13:39 +0700

    Initial commit

---
+++ lib/buoi1.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  /*
+    1 : Kiểu dữ liệu và khai báo biến
+    String text = "My name's phat";
+    int age = 28;
+    double height = 1.75;
+    bool isMale = true;
+
+    String unicode1 = "\u00A4";
+
+    Runes runes = Runes(
+        '\u{2651} \u{2652} \u{2653} \u{2654} '
+    );
+    print(String.fromCharCodes(runes));
+
+    dynamic type
+    comment code : ctrl + / (Không muốn đoạn code thực thi)
+    var text1 = "Xin chào";
+    dynamic data = "Thành công";
+    print(data.runtimeType.toString());
+    data = 200;
+    print(data.runtimeType.toString());
+   */
+}
